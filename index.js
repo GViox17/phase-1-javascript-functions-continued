02bae0a67eb40b5e1f93d4e274c92e64dc04898f
Gabriel Viox <gviox17@gmail.com>
2021-08-30 23:44:06 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code here
+function saturdayFun(activity){
+    return `This Saturday, I want to ${activity || 'roller-skate'}!`
+}
+function mondayWork(activity){
+    return `This Monday, I will ${activity || 'go to the office'}.`
+}
+function wrapAdjective(result){
+    return (adjective) => `You are ${result || '*'}${adjective}${result || '*'}!`
+}
